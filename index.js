da2c9364c3cb918f7a7b3e0feafe10f8cace9374
beatriz <beatrizffeo@gmail.com>
2024-03-18 17:30:34 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    for (const teamRecord of record) {
+        if (teamRecord.result === 'W') {
+            return teamRecord.year;
+        }
+    }
+
+}
